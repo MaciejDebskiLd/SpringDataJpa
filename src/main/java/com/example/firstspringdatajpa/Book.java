c22234e781d7c8e7687a236cef86ed805e1840a8
PatrykIgras <igras.patryk@gmail.com>
2019-10-19 10:48:22 +0200

    create book class

--- src/main/java/com/example/firstspringdatajpa/Book.java
+++ src/main/java/com/example/firstspringdatajpa/Book.java
@@ -17,5 +17,27 @@ public class Book {
         this.title = title;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
